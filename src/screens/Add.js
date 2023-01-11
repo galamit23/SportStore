0292e5b8b4e12bab5d606a9d580a40b2f152e317
galamit23 <gal0688@gmail.com>
2023-01-11 16:37:12 +0200

    creat add com

--- src/screens/Add.js
+++ src/screens/Add.js
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Add() {
   return (
     <div>
-      
+      <from>
+        <label>category:</label>
+        <input></input>
+        
+        <label>name:</label>
+        <input></input>
+
+        <label>pic:</label>
+        <input></input>
+
+        <label>price:</label>
+        <input></input>
+
+      <button className='btn btn-primary'>Add product</button>
+      </from>
     </div>
   )
 }

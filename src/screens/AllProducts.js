49f0d8f47effecd07e82b527fffdd4e7c4b3b676
galamit23 <gal0688@gmail.com>
2023-01-17 13:57:46 +0200

    allProducts

--- src/screens/AllProducts.js
+++ src/screens/AllProducts.js
@@ -54,7 +54,7 @@ export default function AllProducts() {
                     id={task.id}
                     name={task.data.name}
                     price={task.data.price}
-                    <img src={task.data.pic} class="card-img-top" alt="..."> ={task.data.pic}/>
+                    pic= {task.data.pic}/>
                 </div>
             ))}
         </div>
@@ -63,12 +63,12 @@ export default function AllProducts() {
 
 
 
-<div class="card" style="width: 18rem;">
+{/* <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div>
+</div> */}
   

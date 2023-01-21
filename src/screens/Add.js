916001109629713fb368f417b51d229ee8386ae9
Aviel-26 <avielmutai@gmail.com>
2023-01-21 22:04:55 +0200

    Add category options

--- src/screens/Add.js
+++ src/screens/Add.js
@@ -9,7 +9,6 @@ export default function AddTask({ onClose }) {
   const [name, setname] = useState('');
   const [pic, setpic] = useState('');
   const [price, setprice] = useState('');
-  const [inStock, setinStock] = useState('');
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -17,7 +16,7 @@ export default function AddTask({ onClose }) {
     try{
         // addDoc(collection, new document)
         await addDoc(collection(db, 'clothes'), {
-          name: name, pic: pic, price: price, inStock: inStock
+          name: name, pic: pic, price: price
         })
     }catch(error){
         alert(error)
@@ -25,25 +24,30 @@ export default function AddTask({ onClose }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} style={{backgroundColor:'lightgray', padding: 10, marginTop:3}}>
-            <h4 className='text-center'>Add a new task</h4>
-            <label>Add name</label>
+    <form onSubmit={handleSubmit} className="form-group" style={{backgroundColor:'lightgray', padding: 10, marginTop:3}}>
+            <h4 className='text-center'>Add Item</h4>
+            
+            <lable>Category</lable>
+            <br/>
+            <select name="category" id="ct1">
+            <option value="">--Please choose an option--</option>
+            <option value="Clothing">Clothe</option>
+            <option value="Accessories">Accessories</option>
+            <option value="Instruments">Instruments</option>
+            </select>
+            <br/>
+
+            <label>Name</label>
             <input type='text' onChange={(e)=>setname(e.target.value)}
             className='form-control' value={name}/>
-
-            <label>Add price</label>
-            <textarea type='text' onChange={(e)=>setprice(e.target.value)}
-            className='form-control' value={price}></textarea>
             
-            <label>Add pic</label>
-            <textarea type='text' onChange={(e)=>setpic(e.target.value)}
-            className='form-control' value={pic}></textarea>
+            <label>Image</label>
+            <input type='text' onChange={(e)=>setpic(e.target.value)}
+            className='form-control' value={pic}/>
             
-            <label>Add price</label>
-            <textarea type='text' onChange={(e)=>setinStock(e.target.value)}
-            className='form-control' value={inStock}></textarea>
-
-
+            <label>Price</label>
+            <input type='text' onChange={(e)=>setprice(e.target.value)}
+            className='form-control' value={price}/>
             
             <input type='submit' className='btn btn-success mt-2'/>
     </form>
